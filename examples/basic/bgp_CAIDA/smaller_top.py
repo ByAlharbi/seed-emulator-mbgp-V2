84e9b75e906c72bc633b7c5bc44bde129acaafa7
Bashayer Alharbi <baal7013@colorado.edu>
2025-08-27 13:49:16 -0600

    update

--- examples/basic/bgp_CAIDA/smaller_top.py
+++ examples/basic/bgp_CAIDA/smaller_top.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+# We need IX104 for AS135's ring
+    ix104 = base.createInternetExchange(104)#!/usr/bin/env python3
 # encoding: utf-8
 
 from seedemu.layers import Base, Routing, Ebgp, PeerRelationship
@@ -42,14 +43,24 @@ def run(dumpfile = None):
         host = current_as.createHost('host0')
         host.joinNetwork('net0')
 
-    # Create Tier 1 AS group - AS134 components (AS40-45) with ring topology
+    # Create Tier 1 AS groups - 2 groups for debugging
     tier1_connectivity = {
+        # AS134 components (AS40-45) - 6 components forming a ring
         40: ['ix100', 'ix101'],
-        41: ['ix101', 'ix102'], 
+        41: ['ix101', 'ix102'],
         42: ['ix102', 'ix103'],
         43: ['ix103', 'ix105'],
         44: ['ix105', 'ix106'],
-        45: ['ix106', 'ix100']
+        45: ['ix106', 'ix100'],
+        
+        # AS135 components (AS50-56) - 7 components forming a ring  
+        50: ['ix100', 'ix102'],
+        51: ['ix102', 'ix101'],
+        52: ['ix101', 'ix103'],
+        53: ['ix103', 'ix104'],
+        54: ['ix104', 'ix105'],
+        55: ['ix105', 'ix106'],
+        56: ['ix106', 'ix100']
     }
 
     for asn, exchanges in tier1_connectivity.items():
@@ -75,12 +86,13 @@ def run(dumpfile = None):
     for provider, customers in clique_peerings:
         ebgp.addPrivatePeerings(100, [provider], customers, PeerRelationship.Peer)
 
-    # Connect clique ASes to Tier 1 (AS40) as customers
-    # All clique ASes are providers to the Tier 1 network
+    # Connect clique ASes to both Tier 1 groups as customers
     ebgp.addPrivatePeerings(100, clique_ases, [40], PeerRelationship.Provider)
+    ebgp.addPrivatePeerings(100, clique_ases, [50], PeerRelationship.Provider)
 
-    # Connect Tier 1 components in a ring (AS40-45)
-    tier1_ring_connections = [
+    # Connect Tier 1 components in rings
+    # AS134 ring (AS40-45)
+    tier1_as134_connections = [
         (100, 40, 45),  # Close the ring
         (101, 40, 41),
         (102, 41, 42), 
@@ -89,8 +101,25 @@ def run(dumpfile = None):
         (106, 44, 45)
     ]
     
-    for ix, as1, as2 in tier1_ring_connections:
+    for ix, as1, as2 in tier1_as134_connections:
         ebgp.addPrivatePeering(ix, as1, as2, abRelationship=PeerRelationship.Unfiltered)
+        
+    # AS135 ring (AS50-56)
+    tier1_as135_connections = [
+        (100, 50, 56),  # Close the ring
+        (102, 50, 51),
+        (101, 51, 52),
+        (103, 52, 53),
+        (104, 53, 54),
+        (105, 54, 55),
+        (106, 55, 56)
+    ]
+    
+    for ix, as1, as2 in tier1_as135_connections:
+        ebgp.addPrivatePeering(ix, as1, as2, abRelationship=PeerRelationship.Unfiltered)
+
+    # Tier 1 to Tier 1 peering - this might be where loops form!
+    ebgp.addPrivatePeering(100, 40, 50, abRelationship=PeerRelationship.Peer)
 
     ###############################################################################
     # Rendering
